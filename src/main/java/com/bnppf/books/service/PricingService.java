5b76a55facd948b171daad981fe78e430b115ddf
badrkacimi <badrvkacimi@gmail.com>
2024-10-30 12:39:58 +0100

    different basic combinations of test

--- src/main/java/com/bnppf/books/service/PricingService.java
+++ src/main/java/com/bnppf/books/service/PricingService.java
@@ -3,13 +3,19 @@ package com.bnppf.books.service;
 public class PricingService {
 
     private static final double BOOK_PRICE = 50.0;
-    private static final double[] DISCOUNTS = {1.0, 0.95, 0.9, 0.8, 0.75};
+    private static final double[] DISCOUNTS = {0, 1.0, 0.95, 0.9, 0.8, 0.75};
 
     public double calculatePrice(int[] basket) {
         double totalPrice = 0;
+        double discountedPrice;
+        int size = basket.length;
+
         for (int basketItem : basket) {
-            totalPrice = basketItem * BOOK_PRICE;
+            totalPrice += basketItem * BOOK_PRICE;
         }
-        return totalPrice;
+
+        discountedPrice = totalPrice * DISCOUNTS[size];
+
+        return discountedPrice;
     }
 }
--- src/test/java/com/bnppf/books/service/PricingServiceTest.java
+++ src/test/java/com/bnppf/books/service/PricingServiceTest.java
@@ -9,20 +9,33 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class BookServiceTest {
+public class PricingServiceTest {
 
     @InjectMocks
     PricingService pricingService;
+
     @Test
-    public void calculatePrice_SingleBook_NoDiscount(){
-        double price = pricingService.calculatePrice(new int[] {1});
+    public void calculatePrice_SingleBook_NoDiscount() {
+        double price = pricingService.calculatePrice(new int[]{1});
         assertEquals(50.0, price, "Price for a single book should be 50.0");
     }
 
     @Test
     public void calculatePrice_TwoSameBooks_NoDiscount() {
-        double price = pricingService.calculatePrice(new int[] {2});
+        double price = pricingService.calculatePrice(new int[]{2});
         assertEquals(100.0, price, "Price for a two same book should be 100.0");
 
+    }
+
+    @Test
+    void calculatePrice_TwoDifferentBooks_AppliesDiscount() {
+        double price = pricingService.calculatePrice(new int[]{1, 1});
+        assertEquals(95, price);
+    }
+
+    @Test
+    void calculatePrice_FiveDifferentBooks_AppliesDiscount() {
+        double price = pricingService.calculatePrice(new int[]{1, 1, 1, 1, 1});
+        assertEquals(187.5, price);
     }
 }
